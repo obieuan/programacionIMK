bf6f978d84240acda18ca34f160596b643addc9f
obieuan <obieuan@gmail.com>
2022-09-05 12:09:58 -0500

    hola mundo

---
+++ 0x01_holamundo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* Este es mi primer codigo */
+
+int main()
+{
+    printf("Hola, mundo! \n");
+    return 0;
+}
+
---
+++ holamundo.c
@@ -0,0 +1,10 @@
+
+
+/* Este es mi primer codigo */
+
+int main()
+{
+    printf("Hola, mundo!");
+    return 0;
+}
+
