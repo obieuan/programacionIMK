07f2d2fbd86ace7e923567ec1e3b724fc1b75964
obieuan <obieuan@gmail.com>
2022-09-12 12:10:10 -0500

    clase

---
+++ 0x02_Entradas/1-entrada.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+
+int main() 
+{ 
+  char letra;
+  printf("Ingrese un caracter: \n"); 
+  scanf("%c", &letra); 
+  printf("Usted ha escrito la letra: %c\n", letra); 
+  return 0; 
+} 
---
+++ 0x02_Entradas/2-pedir_numero.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+
+int main() 
+{ 
+  int numero;
+  printf("Ingrese un numero: \n"); 
+  scanf("%d", &numero); 
+  printf("Usted ha escrito el numero: %d\n", numero); 
+  return 0; 
+} 
