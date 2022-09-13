7b832f5ecd961f90483c1f90907686a002d493c0
obieuan <obieuan@gmail.com>
2022-09-13 02:18:29 -0500

    update

--- 0x01_Funciones/main.h
+++ 0x01_Funciones/main.h
@@ -1,11 +1,12 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
-void print_alphabet(void);
+void print_alphabet(void); 
 int add(int a, int b);
-int resta(int a, int b);
-int mul(int a, int b);
-float div(int a, int b);
-float mod(int a, int b);
+int resta(int a, int b);        
+
+int mul(int a, int b);          // prototipo de 4-mul.c
+float div(int a, int b);        // prototipo de 5-div.c
+float mod(int a, int b);        // prototipo de 6-div.c
 
 #endif
